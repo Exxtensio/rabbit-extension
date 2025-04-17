9f3716813d03481382e05120f7de9262109aaff7
vladlen-beilik <vladlen.beilik@gmail.com>
2025-04-17 08:32:20 +0200

    init

--- config/rabbit-extension.php
+++ config/rabbit-extension.php
@@ -4,5 +4,5 @@ return [
     'host' => env('RABBITMQ_HOST'),
     'port' => env('RABBITMQ_PORT'),
     'user' => env('RABBITMQ_USER'),
-    'password' => env('RABBITMQ_PASSWORD'),
+    'password' => env('RABBITMQ_PASSWORD')
 ];

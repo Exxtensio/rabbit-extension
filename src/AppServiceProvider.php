827bf35825c5bf36cff98d33a95698caca100446
vladlen-beilik <vladlen.beilik@gmail.com>
2025-05-09 09:12:06 +0200

    ref

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -14,8 +14,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(Filesystem $filesystem): void
     {
-        if (!$filesystem->exists(config_path('rabbit-extension.php'))) {
+        if (!$filesystem->exists(config_path('rabbit-extension.php')))
             $filesystem->copy(__DIR__.'/../config/rabbit-extension.php', config_path('rabbit-extension.php'));
-        }
     }
 }

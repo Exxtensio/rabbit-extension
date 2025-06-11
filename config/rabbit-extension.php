bc5d085937e4f16cddf60494a15e713c380aeeae
vladlen-beilik <vladlen.beilik@gmail.com>
2025-06-11 08:52:30 +0200

    ref

--- config/rabbit-extension.php
+++ config/rabbit-extension.php
@@ -1,12 +1,5 @@
 <?php
 
-$secretsManager = app(\App\Services\AWSSecretsManagerService::class);
-$secrets = $secretsManager->getSecret('rabbit');
-$_ENV['RABBITMQ_HOST'] = $secrets['RABBITMQ_HOST'];
-$_ENV['RABBITMQ_PORT'] = $secrets['RABBITMQ_PORT'];
-$_ENV['RABBITMQ_USER'] = $secrets['RABBITMQ_USER'];
-$_ENV['RABBITMQ_PASSWORD'] = $secrets['RABBITMQ_PASSWORD'];
-
 return [
     'host' => env('RABBITMQ_HOST'),
     'port' => env('RABBITMQ_PORT'),
